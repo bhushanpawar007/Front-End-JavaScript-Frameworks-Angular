eb330973c03e36666ff2a68f0ea278952042d7cd
bhushanpawarj <bhushanpawarj@gmail.com>
2018-12-23 10:47:52 -0800

    Added router module and implemented it for home and menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,25 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from "./services/dish.service";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -26,7 +41,8 @@ import { DishService } from "./services/dish.service";
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
