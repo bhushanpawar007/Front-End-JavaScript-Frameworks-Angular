3c9236c44b328590bbea08e1e8fc7a0f0fd53f09
bhushanpawarj <bhushanpawarj@gmail.com>
2018-12-22 16:42:53 -0800

    Configured Angular with prebuilt material theme and toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
