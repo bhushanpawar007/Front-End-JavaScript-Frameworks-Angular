6865d8a4d68f9240f02e3b2c7fd566fee5c62f34
bhushanpawarj <bhushanpawarj@gmail.com>
2018-12-22 17:57:44 -0800

    Added material grid list and card component  from angular material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material';
 import {FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
-
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, FlexLayoutModule],
+  declarations: [AppComponent, MenuComponent],
+  imports: [
+    BrowserModule,
+     BrowserAnimationsModule, 
+     MatToolbarModule, 
+     FlexLayoutModule,
+      MatListModule, 
+      MatGridListModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
